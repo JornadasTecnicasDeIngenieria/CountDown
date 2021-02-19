e78ccb42cb7d10032d1ab3c81a386a848236c509
AdrianGlez18 <45470685+AdrianGlez18@users.noreply.github.com>
2021-02-19 02:18:59 -0800

    Delete .nuxt directory

--- .nuxt/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Content } from '../../components/Content.vue'
-export { default as Footer } from '../../components/Footer.vue'
-export { default as Header } from '../../components/Header.vue'
-
-export const LazyContent = import('../../components/Content.vue' /* webpackChunkName: "components/content" */).then(c => c.default || c)
-export const LazyFooter = import('../../components/Footer.vue' /* webpackChunkName: "components/footer" */).then(c => c.default || c)
-export const LazyHeader = import('../../components/Header.vue' /* webpackChunkName: "components/header" */).then(c => c.default || c)
